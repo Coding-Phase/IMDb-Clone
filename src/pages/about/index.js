2d8efa1cb972820a881e6a995434136828852d8e
Vishal Thakur <vishal.thakur@ethinos.com>
2023-03-10 17:25:48 +0530

    added about page

---
+++ src/pages/about/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+const About = () => {
+	return (
+		<>
+			<Head>
+				<title>IMDB Clone</title>
+				<meta name="description" content="Created by Vishal Thakur" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div>About</div>;
+		</>
+	);
+};
+
+export default About;
