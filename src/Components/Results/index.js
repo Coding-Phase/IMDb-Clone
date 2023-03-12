34eed912cbc9cadefde26d9d68711f0fee093d5c
Vishal Thakur <vishal.thakur@ethinos.com>
2023-03-12 11:41:23 +0530

    Seo added

--- src/Components/Results/index.js
+++ src/Components/Results/index.js
@@ -1,14 +1,24 @@
+import Head from "next/head";
 import React from "react";
 import Card from "./Card";
 
 const results = ({ results }) => {
 	// console.log(results);
 	return (
-		<div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
-			{results.map((result) => (
-				<Card key={result.id} result={result} />
-			))}
-		</div>
+		<>
+			<Head>
+				<title>IMDB Results</title>
+				<meta name="description" content="Created by Vishal Thakur" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
+				{results.map((result) => (
+					<Card key={result.id} result={result} />
+				))}
+			</div>
+		</>
 	);
 };
 
